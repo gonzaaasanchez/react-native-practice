73b5eb3c01368c3a7cac72345ecf649e7396b0c3
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2024-07-26 20:06:30 -0300

    ADD pressed style

--- components/GoalItem.tsx
+++ components/GoalItem.tsx
@@ -1,4 +1,5 @@
 import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
 
 function GoalItem(
   this: any,
@@ -9,11 +10,14 @@ function GoalItem(
   }
 ) {
   return (
-    <Pressable onPress={props.onDeleteGoal.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#dddddd' }}
+        style={({ pressed }) => pressed && styles.pressedItem}
+        onPress={props.onDeleteGoal.bind(this, props.id)}>
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -22,11 +26,14 @@ export default GoalItem;
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
+    padding: 8,
     color: 'white',
   },
 });
