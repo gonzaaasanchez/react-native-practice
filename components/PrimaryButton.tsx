376c351b21c1ed785c21d012be168c06682a71bc
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2024-08-01 15:26:45 -0300

    ADD buttons styles

---
+++ components/PrimaryButton.tsx
@@ -0,0 +1,49 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+
+interface PrimaryButtonProps {
+  children: React.ReactNode;
+}
+
+function PrimaryButton(props: { children: string }) {
+  function onprogress() {
+    console.log('pressed');
+  }
+
+  return (
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnercontainer, styles.pressed]
+            : styles.buttonInnercontainer
+        }
+        onPress={onprogress}
+        android_ripple={{ color: '#640233' }}>
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: 'hidden',
+  },
+  buttonInnercontainer: {
+    backgroundColor: '#72063c',
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
---
+++ screens/StartGameScreen.tsx
@@ -0,0 +1,44 @@
+import { TextInput, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import PrimaryButton from '../components/PrimaryButton';
+function StartGameScreen() {
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+      />
+      <PrimaryButton>Reset</PrimaryButton>
+      <PrimaryButton>Confirm</PrimaryButton>
+    </View>
+  );
+}
+
+export default StartGameScreen;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginTop: 100,
+    marginHorizontal: 24,
+    padding: 16,
+    backgroundColor: '#4e0329',
+    borderRadius: 8,
+    elevation: 4, // only android
+    shadowColor: 'black',
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 16,
+    shadowOpacity: 0.25,
+  },
+  numberInput: {
+    height: 50,
+    width: 50,
+    fontSize: 32,
+    fontWeight: 'bold',
+    borderBottomColor: '#ddb52f',
+    borderBottomWidth: 2,
+    color: '#ddb52f',
+    marginVertical: 8,
+    textAlign: 'center',
+  },
+});
