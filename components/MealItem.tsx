e63f3db8979b98c9b359181ecec4f539efac8cf4
Gonzalo Sanchez <gonza.sanchez@gmail.com>
2024-08-19 12:28:26 -0300

    ADD item style

--- components/MealItem.tsx
+++ components/MealItem.tsx
@@ -1,4 +1,11 @@
-import { View, Text } from 'react-native';
+import {
+  View,
+  Text,
+  Pressable,
+  Image,
+  StyleSheet,
+  Platform,
+} from 'react-native';
 import Meal from '../models/meal';
 
 type MealItemProps = {
@@ -7,10 +14,72 @@ type MealItemProps = {
 
 const MealItem: React.FC<MealItemProps> = ({ item }) => {
   return (
-    <View>
-      <Text>{item.title}</Text>
+    <View style={styles.mealItem}>
+      <Pressable
+        android_ripple={{ color: '#ccc' }}
+        style={styles.buttonPressed}>
+        <View style={styles.innerContainer}>
+          <View>
+            <Image
+              source={{ uri: item.imageUrl }}
+              style={styles.image}
+            />
+            <Text style={styles.title}>{item.title}</Text>
+          </View>
+          <View style={styles.details}>
+            <Text style={styles.detailItem}>{item.duration}m</Text>
+            <Text style={styles.detailItem}>
+              {item.complexity.toUpperCase()}
+            </Text>
+            <Text style={styles.detailItem}>
+              {item.affordability.toUpperCase()}
+            </Text>
+          </View>
+        </View>
+      </Pressable>
     </View>
   );
 };
 
 export default MealItem;
+
+const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    backgroundColor: 'white',
+    elevation: 4,
+    shadowColor: 'black',
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === 'android' ? 'hidden' : 'visible',
+  },
+  innerContainer: {
+    borderRadius: 8,
+    overflow: 'hidden',
+  },
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  title: {
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+});
